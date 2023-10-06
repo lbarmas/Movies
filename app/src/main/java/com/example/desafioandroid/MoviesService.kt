56598c57d26f5b47af54a8161b6bc3d86e9e45e9
Liset Armas <liset.armasaguila@ypsomed.com>
2023-10-06 18:17:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/desafioandroid/MoviesService.kt
@@ -0,0 +1,8 @@
+package com.example.desafioandroid
+
+import retrofit2.http.GET
+
+interface MoviesService {
+    @GET("discover/movie?api_key=d30e1f350220f9aad6c4110df385d380") //Peticiones al servicio
+   suspend fun getMovies(): MovieResult //Peticiones en el mismo hilo
+}
