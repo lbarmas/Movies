24810c07c59947f2fa1dc4393d805430446323da
Liset Armas <liset.armasaguila@ypsomed.com>
2023-10-07 14:19:58 +0200

    Add Database, DAO, entity

--- app/src/main/java/com/example/desafioandroid/data/local/MoviesDatabase.kt
+++ app/src/main/java/com/example/desafioandroid/data/local/MoviesDatabase.kt
@@ -1,4 +1,30 @@
 package com.example.desafioandroid.data.local
 
-class MoviesDatabase {
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.Query
+import androidx.room.Update
+
+@Database(entities = [LocalMovie::class], version = 1)
+abstract class MoviesDatabase {
+    abstract fun moviesDao(): MoviesDao
 }
+@Dao
+interface MoviesDao {
+    @Query("SELECT * FROM LocalMovie")
+    fun getMovies(): List<LocalMovie>
+    @Update
+    fun updateMovie(movie: LocalMovie)
+    fun getMovie(id: Int): LocalMovie
+
+    fun deleteMovie(movie: LocalMovie)
+}
+@Entity
+data class LocalMovie(
+    val id: Int,
+    val overview: String,
+    val title: String,
+    val poster_path: String,
+    val favorite: Boolean = false
+)
